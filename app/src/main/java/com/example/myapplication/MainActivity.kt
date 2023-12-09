53949c25449a7841abdeee87f38552883e67bf88
Kamila Utezhanova <kamila.utezhanovak@gmail.com>
2023-12-09 15:23:13 +0300

    buttons commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    EditText editText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.editText)
+    }
+
+    public void clickNumber(View view) {
+        String number = editText.getText().toString();
+
     }
 }
